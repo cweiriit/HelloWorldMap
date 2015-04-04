97c661c20ffd2947953c9d2da1cd58efb2f583f0
cweiriit <cweiriit@gmail.com>
2015-04-04 00:00:30 -0500

    Initial commit
    This commit contains the first seemingly complete functional version of
    the app.

---
+++ map/DetailViewController.swift
@@ -0,0 +1,90 @@
+//
+//  DetailViewController.swift
+//  map
+//
+//  Created by Clinton Weir on 4/2/15.
+//  Copyright (c) 2015 HelloWorld-ClintonW. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController, MKMapViewDelegate {
+    var location : CWMapLocation?
+    
+    @IBOutlet weak var theMap : MKMapView?
+    @IBOutlet weak var theTextField : UILabel?
+    @IBOutlet weak var theImage : UIImageView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if location != nil  {
+            if theTextField != nil  {
+                theTextField!.text  = location!.detailText()
+                theTextField!.adjustsFontSizeToFitWidth = true
+//                theTextField!.sizeToFit()
+            }
+            
+            if location!.image != nil && theImage != nil  {
+                theImage!.image = location!.image
+            }
+            
+            if theMap != nil    {
+                theMap!.removeAnnotations(theMap!.annotations)
+                theMap!.addAnnotation(location!.annotation())
+                var coords  = CLLocationCoordinate2D(latitude: location!.locLat, longitude: location!.locLon)
+                theMap!.setRegion(MKCoordinateRegionMake(coords, MKCoordinateSpanMake(0.01, 0.01)), animated: false)
+                theMap!.zoomEnabled     = false
+                theMap!.scrollEnabled   = false
+                theMap!.rotateEnabled   = false
+            }
+        }
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var output : MKAnnotationView   = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        output.image    = UIImage(contentsOfFile: NSBundle.mainBundle().pathForResource("HWMapMarker", ofType: "png")!)
+        return output
+    }
+
+    @IBAction func callNumber(sender: UIButton) {
+        if location != nil  {
+            var numberToCall    = location!.phone.stringByReplacingOccurrencesOfString(" ", withString: "", options: NSStringCompareOptions.allZeros, range: nil)
+            
+            var url = NSURL(string: "tel://" + numberToCall)
+            if url != nil   {
+                UIApplication.sharedApplication().openURL(url!)
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func getDirections(sender: UIButton) {
+        var coords  = CLLocationCoordinate2D(latitude: location!.locLat, longitude: location!.locLon)
+        var place : MKPlacemark = MKPlacemark(coordinate: coords, addressDictionary: nil)
+        var destination : MKMapItem = MKMapItem(placemark: place)
+        destination.name    = location!.name
+        var options = NSDictionary(objectsAndKeys: MKLaunchOptionsDirectionsModeDriving, MKLaunchOptionsDirectionsModeKey)
+        MKMapItem.openMapsWithItems([destination], launchOptions: options)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ map/ViewController.swift
@@ -0,0 +1,212 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by Clinton Weir on 4/1/15.
+//  Copyright (c) 2015 HelloWorld-ClintonW. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var theMap : MKMapView?
+    @IBOutlet weak var theTable : UITableView?
+    var theLocations : [String : CWMapLocation] = Dictionary()
+    var theLocationsArray : [CWMapLocation] = Array()
+    var locationManager : CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.AuthorizedAlways ||
+            status == CLAuthorizationStatus.AuthorizedWhenInUse    {
+                NSLog("Got authorization!")
+                manager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManagerDidPauseLocationUpdates(manager: CLLocationManager!) {
+    }
+    
+    func locationManagerDidResumeLocationUpdates(manager: CLLocationManager!) {
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        NSLog("Got new location data")
+        calculateDistances(newLocation)
+    }
+    
+    func calculateDistances(aLocation : CLLocation?)   {
+        var newLocation = aLocation
+        if newLocation == nil && locationManager != nil   {
+            newLocation = locationManager!.location
+        }
+        for (key, location) : (String, CWMapLocation) in theLocations    {
+            location.distance(newLocation!)
+        }
+        
+        for var index=0; index<theLocationsArray.count; index++ {
+            var location1   = theLocationsArray[index]
+            var distance1   = location1.lastDistance
+            
+            for var indey=index+1; indey<theLocationsArray.count; indey++   {
+                var location2   = theLocationsArray[indey]
+                var distance2   = location2.lastDistance
+                
+                if distance2 < distance1  {
+                    var temp2   = location2
+                    theLocationsArray.removeAtIndex(indey)
+                    theLocationsArray.insert(temp2, atIndex: index)
+                    NSLog("Moving \(location2.name) ahead of \(location1.name)")
+                    
+                    location1   = location2
+                    distance1   = distance2
+                }
+            }
+        }
+        
+        theTable?.reloadData()
+        //theTable?.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Automatic)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didDetermineState state: CLRegionState, forRegion region: CLRegion!) {
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var output : MKAnnotationView   = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        output.image    = UIImage(contentsOfFile: NSBundle.mainBundle().pathForResource("HWMapMarker", ofType: "png")!)
+        return output
+    }
+    
+    func mapViewDidFinishLoadingMap(mapView: MKMapView!) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0))   {
+            var locations : [CWMapLocation]  = CWMapLocation.getLocs()
+            
+            for location : CWMapLocation in locations   {
+                dispatch_async(dispatch_get_main_queue())   {
+                    mapView.addAnnotation(location.annotation())
+                }
+                self.theLocations[location.name] = location
+                self.theLocationsArray.append(location)
+            }
+            
+            self.locationManager = CLLocationManager()
+            self.locationManager!.delegate   = self
+            self.locationManager!.desiredAccuracy    = kCLLocationAccuracyKilometer
+            self.locationManager!.distanceFilter     = 500
+            
+            if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.NotDetermined  {
+                NSLog("requesting location auth")
+                self.locationManager!.requestWhenInUseAuthorization()
+            }
+            
+            dispatch_async(dispatch_get_main_queue())   {
+                if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedAlways ||
+                    CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse    {
+                        self.locationManager!.startUpdatingLocation()
+                        self.calculateDistances(nil)
+                }
+                else    {
+                    self.theTable!.reloadData()
+                }
+                
+                
+            }
+        }
+    }
+    
+    /*
+    func distanceTo(location : CWMapLocation) -> CLLocationDistance   {
+        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedAlways || CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse && CLLocationManager.locationServicesEnabled()    {
+            var currentLocation = locationManager!.location
+            return location.distance(currentLocation)
+        }
+        
+        return CLLocationDistance.infinity
+    }   */
+    
+    
+    
+    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
+        var location : CWMapLocation    = theLocations[view.annotation.title!]!
+        var message = location.address()
+        
+        var distMsg = location.distanceString()
+        if distMsg != nil   {
+            message += "\n" + distMsg!
+        }
+        
+        performSegueWithIdentifier("pushDetail", sender: location)
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section != 0 {
+            return 0
+        }
+        else    {
+            NSLog("count: \(theLocations.count)")
+            return theLocations.count
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath) as UITableViewCell
+        
+        var section = indexPath.section
+        var row     = indexPath.row
+        var location            = theLocationsArray[row]
+        
+        
+        cell.textLabel!.text    = location.name
+        
+        var subText    = location.address().stringByReplacingOccurrencesOfString("\n", withString: "; ", options: NSStringCompareOptions.allZeros, range: nil)
+        var distMsg = location.distanceString()
+        if distMsg != nil   {
+            subText += "; " + distMsg!
+        }
+        cell.detailTextLabel!.text  = subText
+        
+        return cell
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var location : CWMapLocation?
+        if let annotationview = sender as? MKAnnotationView {
+            let annotation  = annotationview.annotation
+            location    = theLocations[annotation.title!]
+            NSLog("segue for location \(location?.name) using MKAnnotationView")
+        }
+        else if let cell = sender as? UITableViewCell   {
+            location    = theLocations[cell.textLabel!.text!]
+            NSLog("segue for location \(location?.name) using UITableViewCell")
+        }
+        else if var theLocation = sender as? CWMapLocation {
+            location = theLocation
+        }
+        
+        if location != nil {
+            NSLog("location is \(location!.name)")
+            if let detailVC = segue.destinationViewController as? DetailViewController  {
+                detailVC.location   = location
+            }
+        }
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+}
+
